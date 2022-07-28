acd19ea5c548da42f0ebcfa04c5a2f89f0a145aa
egoavara <egoavara@gamil.com>
2022-07-29 08:09:09 +0900

    API 변경

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,41 @@
 import { FastifyModular } from "fastify-modular"
-import fs from "fs/promises"
-import type { PoolConfig } from "pg"
+import type { PoolConfig as _PoolConfig } from "pg"
 import pg from "pg"
-import { Readable } from "stream"
-import { json } from "stream/consumers"
 
+export type PoolConfig = Pick<_PoolConfig, keyof _PoolConfig>
 export type PgPool = Pick<pg.Pool, keyof pg.Pool>
 export type PgPoolClient = Pick<pg.PoolClient, keyof pg.PoolClient>
-export type PostgresModuleOption =
-    | { file: string, format?: 'json' }
-    | PoolConfig
+export type PostgresModuleOption = PoolConfig
 
 export const PostgresModule = FastifyModular('postgres')
     .option<PostgresModuleOption>()
     // ====================================================================================
-    // pool is instance of pg.Pool
-    .static('pool', 'auto', async ({ }, option) => {
-        if ('file' in option) {
-            let config: PoolConfig
-            switch (option.format) {
-                case 'json':
-                case undefined:
-                    config = await json(Readable.from(await fs.readFile(option.file))) as any
-                    break
-                default:
-                    throw new Error(`[PostgresModule] unknown format '${option.format}'`)
-                    break
-            }
-            return new pg.Pool(config) as PgPool
-        }
+    // pg is instance of pg.Pool
+    .static('pg', 'auto', async ({ }, option) => {
         return new pg.Pool(option) as PgPool
     })
     // ====================================================================================
     // transaction is instance of pg.PoolClient
     // pool connection automatically get and release connection per request(HTTP, WS)
-    .dynamic("transaction", 5000,
-        async ({ pool }) => {
-            const tx = await pool.connect() as PgPoolClient
+    .dynamic("txPg:raw", 5000,
+        async ({ pg }) => {
+            const tx = await pg.connect() as PgPoolClient
+            await tx.query("begin");
+            return tx
+        },
+        async ({ value, catched }) => {
+            if (catched === undefined) {
+                await value.query("commit");
+                value.release()
+            } else {
+                await value.query("rollback");
+                value.release()
+            }
+        }
+    )
+    .dynamic("txPg", 5000,
+        async ({ "txPg:raw":txPgRaw }) => {
+            const tx = await txPgRaw as PgPoolClient
             await tx.query("begin");
             return tx
         },
@@ -52,18 +51,16 @@ export const PostgresModule = FastifyModular('postgres')
     )
     // ====================================================================================
     // when sse, ws api, it support rollback, commit and release socket
-    .when({ includes: ['sse', 'ws'] })
-    .define("rollback", async ({ transaction }) => {
-        if (!transaction.isLoad) { return }
-        await (await transaction).query("rollback");
-        await transaction.drop()
+    .dynamic("txPgRollback", 5000, async ({ txPg }) => {
+        if (!txPg.isLoad) { return }
+        await (await txPg).query("rollback");
+        await txPg.drop()
     })
-    .define("commit", async ({ transaction }) => {
-        if (!transaction.isLoad) { return }
-        await (await transaction).query("commit");
-        await transaction.drop()
+    .dynamic("txPgCommit", 5000, async ({ txPg }) => {
+        if (!txPg.isLoad) { return }
+        await (await txPg).query("commit");
+        await txPg.drop()
     })
-    .end()
     .build()
 
 export default PostgresModule;
